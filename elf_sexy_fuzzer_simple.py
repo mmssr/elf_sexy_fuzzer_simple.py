158796d24164fdca0d86d2ebafb366fbbbed4415
0x6D6172636F <marcamesser@gmail.com>
2022-12-01 23:39:15 -0500

    Update elf_sexy_fuzzer_simple.py

--- elf_sexy_fuzzer_simple.py
+++ elf_sexy_fuzzer_simple.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python3
+
+import argparse
 import elf
 import random
 
+# Parse the command-line arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument("filepath", help="path to the ELF file")
+parser.add_argument("iterations", type=int, help="number of iterations to run")
+args = parser.parse_args()
+
 # Load the ELF file.
-elf_file = elf.ELF("path/to/elf/file")
+elf_file = elf.ELF(args.filepath)
 
 # Generate a random section name.
 def generate_section_name():
@@ -13,14 +22,15 @@ def generate_section_name():
   return name
 
 # Fuzz the ELF file's section names.
-for section in elf_file.iter_sections():
-  # Generate a random section name.
-  name = generate_section_name()
-  print("Fuzzing section name:", section.name, "->", name)
+for i in range(args.iterations):
+  for section in elf_file.iter_sections():
+    # Generate a random section name.
+    name = generate_section_name()
+    print("Iteration", i, ": Fuzzing section name:", section.name, "->", name)
 
-  # Try to load the ELF file with the new section name.
-  try:
-    section.name = name
-    elf_file.save("path/to/output/file")
-  except Exception as e:
-    print
+    # Try to load the ELF file with the new section name.
+    try:
+      section.name = name
+      elf_file.save("path/to/output/file")
+    except Exception as e:
+      print("Error:", e)
